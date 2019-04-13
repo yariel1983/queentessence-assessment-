4cdf7a3daa2d2698ed8c82eb7386738b1a2b8dff
Yariel1983 <cubanboy4life@gmail.com>
2019-04-13 13:30:18 -0400

    1 hour project

---
+++ footer.php
@@ -0,0 +1,22 @@
+<!--- Footer --->
+
+<!-- Copyright -->
+<footer>
+    <div class="footer-copyright fixed-bottom text-center py-3 bg-dark text-white">© 2019 Copyright: 
+        <img class="img-fluid rounded-circle mr-1" src="assets/images/Me.png" alt="Responsive" width="30px"> Yariel Dominguez UI
+
+    </div>
+    <!-- Copyright -->
+
+</footer>
+
+
+
+<!-- Jquery -->
+
+<script src="assets/js/jquery-3.3.1.slim.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/myscripts.js"></script>
+<script src="assets/js/jquery.validate.min.js"></script>
+
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE  html>
+<html>
+
+    <head>
+        <title>Yariel</title>
+        <!-- CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css" type="text/css" />      
+        <link rel="stylesheet" href="assets/css/fontawesome/css/all.css">
+    </head>
+
+    <body>
+
+        <!-- Navigation -->
+        <nav class="navbar navbar-light bg-dark">
+            
+            <a class="navbar-brand text-white">My Navbar</a>
+           
+            <form class="form-inline">
+                
+                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                
+            </form>
+            
+        </nav>
+
+    </body>
+</html>
